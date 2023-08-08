55a97d8373151f4a1af9f85143473bb8c5584895
iulianchiri <140550697+iulianchiri@users.noreply.github.com>
2023-08-08 19:28:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
 
+// Folosește body-parser pentru a procesa corpul cererilor JSON
 app.use(bodyParser.json());
 
+// Ruta pentru autentificare
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
 
@@ -12,35 +15,7 @@ app.post('/login', (req, res) => {
   // Altfel, vom returna o eroare
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-
-// Folosește body-parser pentru a procesa corpul cererilor JSON
-app.use(bodyParser.json());
-
-// Adaugă rute pentru aplicația ta
-app.get('/', (req, res) => {
-    res.send('Bun venit la serverul nostru!');
-});
-
-// Pornirea serverului pe portul 3000
-app.listen(3000, () => {
-    console.log('Serverul rulează pe portul 3000');
-});
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-
-// Folosește body-parser pentru a procesa corpul cererilor JSON
-app.use(bodyParser.json());
-
-// Adaugă rute pentru aplicația ta
+// Ruta de bază
 app.get('/', (req, res) => {
     res.send('Bun venit la serverul nostru!');
 });
