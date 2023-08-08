0da45a5c3baaf800409cbed39b0d465711416022
iulianchiri <140550697+iulianchiri@users.noreply.github.com>
2023-08-08 19:24:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,3 +15,20 @@ app.post('/login', (req, res) => {
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Folosește body-parser pentru a procesa corpul cererilor JSON
+app.use(bodyParser.json());
+
+// Adaugă rute pentru aplicația ta
+app.get('/', (req, res) => {
+    res.send('Bun venit la serverul nostru!');
+});
+
+// Pornirea serverului pe portul 3000
+app.listen(3000, () => {
+    console.log('Serverul rulează pe portul 3000');
+});
